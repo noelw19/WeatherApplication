afc4e87fdce9e90cabc291a8fd1dd8c106765f00
Noel <Noelw19@outlook.com>
2020-03-26 15:49:07 +1300

    building gui for app

--- colos.js
+++ colos.js
@@ -19,4 +19,37 @@ console.log(message);
 });
 
 
+const form =document.querySelector(".top-banner form");
 
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const inputVal = input.value;
+});
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {})
+    .catch(() => {
+        msg.textContent = "Please search for a valid city";
+    })
+
+    const {main, name, sys, weather} = data ;
+    const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+    const li = document.createElement("li");
+    li.classList.add("city");
+    const markup = `
+    <h2 class="city-name" data-name="${name},${sys.country}">
+    <span>${name}</span>
+    <sup>${sys.country}</sup>
+    </h2>
+    <div class ="city-temp">${math.round(main.temp)}<sup>°C</sup>
+    </div>
+    <figure>
+    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+    <figcaption>${weather[0]["description"]}</figcaption>
+    </figure>
+    `;
+
+    li.innerHTML=markup;
+    list.appendChild(li);
